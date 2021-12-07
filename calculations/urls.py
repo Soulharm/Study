c105c4c75747de90756f0d5035e1cbf92e373a02
Soulharm <soulharm1@gmail.com>
2021-12-07 22:12:48 +0300

    Finished index, started multiplication

--- calculations/urls.py
+++ calculations/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from calculations.views import index
+from calculations.views import *
 
 urlpatterns = [
     path('', index, name='home'),
     path('admin/', admin.site.urls),
+    path('mult/', multiplication, name='multiplication')
 ]
--- calculations/views.py
+++ calculations/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'calculations/index.html',)
+    return render(request, 'calculations/index.html', )
+
+
+def multiplication(request):
+    return render(request, 'calculations/multiplication.html', )
