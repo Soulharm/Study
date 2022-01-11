6c87557475b9f55e171c70fcae5dbd32e19029b6
Soulharm <soulharm1@gmail.com>
2022-01-11 16:56:58 +0300

    Divisions added

--- calculations/functions.py
+++ calculations/functions.py
@@ -1,31 +1,38 @@
 import random
 
 
-def testmultdiv(x):
+def test_mult_div(num, operation_type):
     example = []
-    correctresult = []
-    while len(example) < x:
-        result = mult()
+    correct_result = []
+    while len(example) < num:
+        result = mult() if operation_type == 'mult' else div()
         if result[0] in example:
             continue
         example.append(result[0])
-        correctresult.append(result[1])
-    return example, correctresult
+        correct_result.append(result[1])
+    return example, correct_result
 
 
 def randomnumber():
     num = [2, 3, 4, 5, 6, 7, 8, 9]
-    randomnumber = random.choice(num)
-    return randomnumber
+    random_number = random.choice(num)
+    return random_number
 
 
 def mult():
-    firstnum = randomnumber()
-    secondnum = randomnumber()
-    correctresult = firstnum * secondnum
-    example = str(firstnum) + ' * ' + str(secondnum) + " ="
-    return [example, correctresult]
+    first_num = randomnumber()
+    second_num = randomnumber()
+    correct_result = first_num * second_num
+    example = str(first_num) + ' * ' + str(second_num) + " ="
+    return [example, correct_result]
 
+
+def div():
+    first_num = randomnumber()
+    second_num = randomnumber()
+    correct_result = first_num * second_num
+    example = str(correct_result) + ' : ' + str(first_num) + " ="
+    return [example, second_num]
 
 
 
--- calculations/views.py
+++ calculations/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 
-from calculations.functions import testmultdiv
+from calculations.functions import test_mult_div
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'calculations/index.html', )
 
-def count(request):
-    pass
 
 def multiplication(request):
     # Создаём массивы примеров и правильных решений для рендеринга
-    examples, results = testmultdiv(5)
+    examples, results = test_mult_div(5, 'mult')
     dict_result = dict(zip(examples, results))
     result = {'result': dict_result}
     return render(request, 'calculations/testyourself.html', context=result)
 
 
 def division(request):
-    return render(request, 'calculations/testyourself.html', )
+    # Создаём массивы примеров и правильных решений для рендеринга
+    examples, results = test_mult_div(5, 'div')
+    dict_result = dict(zip(examples, results))
+    result = {'result': dict_result}
+    return render(request, 'calculations/testyourself.html', context=result)
