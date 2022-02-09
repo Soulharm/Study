ef871b32a52145df2df1713fb7e302f7a4941b06
Soulharm <soulharm1@gmail.com>
2022-02-09 16:44:54 +0300

    Added Favicon

--- study_site/settings.py
+++ study_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*xva6^16(&t6fg!z*vnz#05baetj%_sb*z-s#lkjlw2ozu6#3+'
+# SECRET_KEY = 'django-insecure-*xva6^16(&t6fg!z*vnz#05baetj%_sb*z-s#lkjlw2ozu6#3+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
